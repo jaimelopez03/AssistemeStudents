8b7b40c0da5879987621531e7e02122b19190e15
jaimelopez03 <60837707+jaimelopez03@users.noreply.github.com>
2020-06-01 13:58:34 -0700

    Final 2.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { environment } from 'src/enviroments/environment';
 import { Platform, MenuController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
